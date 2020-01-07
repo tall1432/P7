f22a84900e9a52993a93e4253792f2a3a7dcd483
tall1432 <mtall1432@gmail.com>
2020-01-07 08:59:33 +0100

    fix jumbotron

--- src/Jumbotron.js
+++ src/Jumbotron.js
@@ -1,16 +1,19 @@
 import React from 'react'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 export const Jumbotron = () => {
     return (
         <div>
             
+            <Jumbotron>
   <h1>Hello, world!</h1>
   <p>
     This is a simple hero unit, a simple jumbotron-style component for calling
     extra attention to featured content or information.
   </p>
- 
-
+  <p>
+    <Button variant="primary">Learn more</Button>
+  </p>
+</Jumbotron>
         </div>
     )
 }
